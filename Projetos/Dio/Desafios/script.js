e510201ef72d6c0fd17b3fc9656df8e1b1da1e18
r_souza <Rafaelrosod@outlook.com>
2024-05-06 17:48:34 -0300

    Desafio 1, completo.

---
+++ Projetos/Dio/Desafios/script.js
@@ -0,0 +1,8 @@
+
+
+const posicaoInicial = parseInt(gets());
+const totalPassos = parseInt(gets());
+
+const posicaoFinal = posicaoInicial + totalPassos;
+
+console.log("Posicao final do heroi:", posicaoFinal);
